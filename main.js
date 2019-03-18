25ac4d91114f8f6a88aa8fcdde015597fbc4fc1e
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-03-18 01:41:03 -0400

    Add boiling-temp text

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 // It is always helpful to use comments in your code!
+// b i. 212 immediately prints
+const boilingTemp = document.getElementById("boiling-temp");
+
+boilingTemp.textContent += "212";
