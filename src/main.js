c6dc3aba8e332119b6a8804e7509a98487f3f1cb
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-03-22 02:03:18 -0400

    Add files to transpile

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// It is always helpful to use comments in your code!
+// b i. 212 immediately prints
+const boilingTemp = document.getElementById("boiling-temp");
+
+boilingTemp.textContent += "212";
+// b ii. 32 immediately prints
+const freezingTemp = document.getElementById("freezing-temp");
+
+freezingTemp.textContent += "32";
+// b iii. water temp prints
+const waterTemp = document.getElementById("water-temp");
+
+waterTemp.textContent += "80";
+// iv.
+const waterTempNumber = document.getElementsByClassName('hide');
+
+//function toggleHide()
+
+//setTimeout(onLoad,3000);
